1884aaf291335978d695aa31c5158dedc5410813
Richter <fw137404@feuerwehr.cottbus.de>
2019-03-06 15:16:31 +0100

    Update app_cfg.js

--- server/app_cfg.js
+++ server/app_cfg.js
@@ -2,6 +2,7 @@ var app_cfg = {};
 
 app_cfg.global = {
 	webport: 3000,
+	httpsport: 3443,
 	udpport: 60233,
 	database: './database.sqlite3',
 	soundpath: '/public/media/',
@@ -9,12 +10,13 @@ app_cfg.global = {
 	defaultuser: 'me',
 	defaultpass: '123',
 	defaultuserip: '127.0.0.1',
+	ip_auth_range: ['::ffff:172.16.5.0/24', '::ffff:192.168.2.0/24'],
 	saltRounds: 10,
 	sessionsecret: '0987654321abcdef#xyz',
 	// TODO: eindeutige ID/Version für Anwendung hinterlegen
 	// TODO: Karten-URL für Client festlegen
 	app_id: process.pid,
-	map_tile: '/media/maps/tiles_bw/{z}/{x}/{y}.png'
+	map_tile: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 };
 
 module.exports = app_cfg;
