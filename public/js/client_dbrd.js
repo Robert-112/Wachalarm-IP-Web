d9e203296b175c52592513e9d8400c90378d2754
Robert Richter <robertrichter87@web.de>
2020-02-28 17:31:01 +0100

    update Fr 28. Feb 17:31:01 CET 2020

--- public/js/client_dbrd.js
+++ public/js/client_dbrd.js
@@ -79,16 +79,25 @@ var map = L.map('map', {
     item5.className = 'ion-md-star';
     item5.innerHTML = '<a>&nbsp;Klaus (AGT)</a>';
 
+    var item2 = document.createElement('div');
+    item2.className = 'ion-md-star';
+    item2.innerHTML = '<a>&nbsp;Günter (AGT)</a>';
+
     var items = new vis.DataSet([
     {id: 1, group: 0, className: 'ma', content: 'Hans', start: start, end: end},
-    {id: 2, group: 0, className: 'fk', content: 'Günter', start: start2, end: end2},
+    {id: 2, group: 0, className: 'fk-agt', content: item2, start: start2, end: end2},
     {id: 3, group: 1, className: 'ek', content: 'Rudi', start: start3, end: end3},
     {id: 4, group: 1, className: 'ek-agt', content: item5, start: start4, end: end4},
     {id: 5, group: 1, className: 'ma', content: 'Jürgen', start: start5, end: end5},
     {id: 6, group: 1, className: 'ek', content: 'Florian', start: start6, end: end6},
     ]);
     // Configuration for the Timeline
-    var options = {};
+    var options = {
+      rollingMode: {
+        follow: true,
+        offset: 0.25
+      }
+    };
     // Create a Timeline
     var timeline = new vis.Timeline(container, items, options);
     timeline.setGroups(groups);
@@ -118,3 +127,4 @@ var map = L.map('map', {
       id2
     );
     timeline.setCustomTimeMarker(markerText, id2, false);
+
