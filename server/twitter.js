cc8816a7f60f9b0848ccb3fc0366f33b6e12d6dd
Robert Richter <robertrichter87@web.de>
2020-03-11 23:31:02 +0100

    update Mi 11. Mär 23:31:01 CET 2020

--- server/twitter.js
+++ server/twitter.js
@@ -10,27 +10,23 @@ module.exports = function(twitter, uuidv4, app_cfg) {
       access_token_secret: twitter_data.tw_access_token_secret
     });
     
-    /*var params = {owner_screen_name: twitter_data.tw_screen_name, slug: twitter_data.list, count: 50};
-    client.get('lists/members', params, function(error, members, response) {
-      if (!error) {
-        console.log(members);
-        callback && callback(members);
-      }  else {
-        console.log(error);
-        callback && callback(null);
-      };
-    });*/
-
-    var params = {screen_name: twitter_data.tw_screen_name};
+      var params = {screen_name: twitter_data.tw_screen_name};
     client.get('lists/list', params, function(error, lists, response) {
       if (!error) {
         
-
         var list_obj = lists.filter(function(o){return o.name == twitter_data.list;} );
-        console.log(JSON.stringify(list_obj));
-        console.log(list_obj[0].id);
-
-        callback && callback(lists);
+        //console.log(JSON.stringify(list_obj));
+        console.log(list_obj[0].id_str);
+        var member_params = {list_id: list_obj[0].id_str, count: 50};
+        client.get('lists/members', member_params, function(error, members, response) {
+          if (!error) {
+            console.log(JSON.stringify(members));
+            callback && callback(members);
+          } else {
+            console.log(error);
+            callback && callback(null);
+          };
+        });
       }  else {
         console.log(error);
         callback && callback(null);
