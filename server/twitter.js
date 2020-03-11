0618d378242232d13080c0b7bc85015213f0dbc9
Robert Richter <robertrichter87@web.de>
2020-03-11 23:01:01 +0100

    update Mi 11. Mär 23:01:01 CET 2020

--- server/twitter.js
+++ server/twitter.js
@@ -10,7 +10,7 @@ module.exports = function(twitter, uuidv4, app_cfg) {
       access_token_secret: twitter_data.tw_access_token_secret
     });
     
-    var params = {list_id: 1237715592234315778};//owner_screen_name: twitter_data.tw_screen_name, slug: twitter_data.list, count: 50};
+    /*var params = {owner_screen_name: twitter_data.tw_screen_name, slug: twitter_data.list, count: 50};
     client.get('lists/members', params, function(error, members, response) {
       if (!error) {
         console.log(members);
@@ -19,7 +19,25 @@ module.exports = function(twitter, uuidv4, app_cfg) {
         console.log(error);
         callback && callback(null);
       };
+    });*/
+
+    var params = {screen_name: twitter_data.tw_screen_name};
+    client.get('lists/list', params, function(error, lists, response) {
+      if (!error) {
+        
+
+        var list_obj = lists.filter(function(o){return o.name == twitter_data.list;} );
+        console.log(JSON.stringify(list_obj));
+        console.log(list_obj[0].id);
+
+        callback && callback(lists);
+      }  else {
+        console.log(error);
+        callback && callback(null);
+      };
     });
+
+
   };
 
   return {
