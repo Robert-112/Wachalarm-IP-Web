a9a85843b0f89020f3bdb17ee630967d834996d7
Robert Richter <robertrichter87@web.de>
2020-04-29 11:01:01 +0200

    update Mi 29. Apr 11:01:01 CEST 2020

--- public/js/client_waip.js
+++ public/js/client_waip.js
@@ -472,6 +472,55 @@ function set_clock() {
 // Uhrzeit jede Sekunden anpassen
 setInterval(set_clock, 1000);
 
+$(document).ready(function(){
+  animateDiv();
+  
+});
+
+function makeNewPosition(){
+  
+  // Get viewport dimensions (remove the dimension of the div)
+  var h = $('.fullheight').height();// - 50;
+  var w = $('.fullheight').width();// - 50;
+  
+  var nh = Math.floor(Math.random() * h);
+  var nw = Math.floor(Math.random() * w);
+  
+  return [nh,nw];    
+  
+}
+
+function animateDiv(){
+  var newq = makeNewPosition();
+  var oldq = $('.clock_x').offset();
+  var speed = calcSpeed([oldq.top, oldq.left], newq);
+  
+  $('.clock_x').animate({ top: newq[0], left: newq[1] }, speed, function(){
+    animateDiv();        
+  });
+  
+};
+
+function calcSpeed(prev, next) {
+  
+  var x = Math.abs(prev[1] - next[1]);
+  var y = Math.abs(prev[0] - next[0]);
+  
+  var greatest = x > y ? x : y;
+  
+  var speedModifier = 0.1;
+
+  var speed = Math.ceil(greatest/speedModifier);
+
+  return speed;
+
+}
+
+
+
+
+
+
 /* ########################### */
 /* ####### Rückmeldung ####### */
 /* ########################### */
