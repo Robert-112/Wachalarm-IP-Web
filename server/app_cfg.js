3d6ba81215c48114514b98927900f1a8a3a17139
Robert Richter <robertrichter87@web.de>
2020-05-07 13:31:01 +0200

    update Do 7. Mai 13:31:01 CEST 2020

--- server/app_cfg.js
+++ server/app_cfg.js
@@ -16,6 +16,8 @@ app_cfg.global = {
 	ip_auth_range: ['::ffff:172.16.5.0/24', '::ffff:192.168.2.0/24'],
 	saltRounds: 10,
 	sessionsecret: '0987654321abcdef#xyz',
+	backup_rmld: true,
+	backup_path: '/bkp/',
 	app_id: process.pid
 };
 
@@ -24,14 +26,27 @@ app_cfg.public = {
 	company: 'Leitstelle Lausitz',
 	version: 'Version 1.2',
 	map_tile: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-	map_attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	map_attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	// TODO: Link Impressium
-	// TODO: Link Datenschutzerklärung
+	own_imprint: false,
+	url_imprint: 'https://www.nix.nix/impressium',
+	// TODO: Link Datenschutzerklärung, TLF-Sofort
+	own_privacy: false,
+	url_privacy: 'https://www.nix.nix/datenschutz'
+};
+
+app_cfg.api = {
+	secret: 'asdfwert1234567890#',
+	access_list: ['192.168.2.20', '192.168.2.30']
 };
 
 app_cfg.remote = {
-	api: 'http://localhost:8078/api',
-	jwt_secret: 'asdfwert1234567890#'
+	endpoint_host: 'localhost',
+	endpoint_port: '8090',
+	endpoint_route: '/api',
+	secret: 'asdfwert1234567890#',
+	allow_mission_type: ['Brandeinsatz', 'Hilfeleistung'],
+	allow_data_type: ['uuid', 'nummer', 'alarmzeit', 'art', 'stichwort', 'sondersignal', 'ort', 'ortsteil', 'wgs84_area']
 };
 
 module.exports = app_cfg;
